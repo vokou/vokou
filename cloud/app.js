980db0496525ff87d4e22f5c39f71f86dc840b46
coderU <jemit.uriel@gmail.com>
2015-09-14 23:33:15 -0700

    Finish search spg

--- cloud/app.js
+++ cloud/app.js
@@ -1,6 +1,10 @@
 
 // These two lines are required to initialize Express in Cloud Code.
+var crypto = require('crypto'),
+
+http = require('http');
 User = require("cloud/users.js");
+var convert = require('cloud/convert.js');
 var _ = require('underscore');
 parseExpressHttpsRedirect = require('parse-express-https-redirect');
 parseExpressCookieSession = require('parse-express-cookie-session');
@@ -12,10 +16,25 @@ app.set('views', 'cloud/views');  // Specify the folder to find templates
 app.set('view engine', 'ejs');    // Set the template engine
 app.use(express.bodyParser());    // Middleware for reading request body
 app.use(parseExpressHttpsRedirect());  // Require user to be on HTTPS.
+algorithm = 'aes-256-ctr',
+password = 'We are fucking SPG';
 app.use(express.cookieParser('We are fucking SPG'));
 app.use(parseExpressCookieSession({ cookie: { maxAge: 3600000 } }));
 
 
+function encrypt(text){
+  var cipher = crypto.createCipher(algorithm,password)
+  var crypted = cipher.update(text,'utf8','hex')
+  crypted += cipher.final('hex');
+  return crypted;
+}
+
+function decrypt(text){
+  var decipher = crypto.createDecipher(algorithm,password)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
+}
 // This is an example of hooking up a request handler with a specific request
 // path and HTTP verb using the Express routing API.
 app.get('/',function(req,res){
@@ -87,6 +106,41 @@ app.get('/mainpage', function(req, res) {
   } else {
     res.send('This is main page');
   }
+});
+
+app.get('/search',function(req,res){
+  //Date format: mm/dd/yyyy
+  if( !req.query.country ){
+    return res.status(500).send("Country wrong");
+  }else if( !req.query.checkin ){
+    return res.status(500).send("checkin Wrong");
+  }else if( !req.query.checkout ){
+    return res.status(500).send("checkout Wrong");
+  }else if( !req.query.city){
+    return res.status(500).send("city Wrong");
+  }
+  req.query.country = req.query.country.toLowerCase();
+  req.query.country = convert[req.query.country];
+  if (req.query.country == undefined){
+    return res.status(500).send("Country wrong")
+  }
+  country = req.query.country;
+  state = '';
+  if(country == 'us'){
+    if( !req.query.state){
+      return res.status(500).send("state Wrong");
+    }
+    state = convert[req.query.state.toLowerCase()];
+  }
+  arrivalDate = req.query.checkin;
+  departureDate = req.query.checkout;
+  city = req.query.city.replace(' ', '+');
+  spgsource = "http://www.starwoodhotels.com/preferredguest/search/results/grid.html?localeCode=en_US&city=" + city + "&stateCode=" + state + "&countryCode=" + country + "&searchType=location&hotelName=&"+"currencyCode=USD&arrivalDate=" + arrivalDate + "&departureDate=" + departureDate + "&numberOfRooms=1&numberOfAdults=1&numberOfChildren=0&iataNumber=";
+  res.end(encrypt(spgsource));
+
+
+
+  //
 });
 // // Example reading from the request query string of an HTTP get request.
 // app.get('/test', function(req, res) {
