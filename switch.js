da07eac8f06b8f71c837d70021a1c0ac7e0c5471
LASkuma <jimmyx@me.com>
2015-10-24 21:11:18 -0400

    Modify switch

--- switch.js
+++ switch.js
@@ -1,5 +1,8 @@
+"use strict";
+
 var fs = require('fs');
-var stream = fs.createWriteStream(".parse.local");
+var parseStream = fs.createWriteStream(".parse.local");
+
 var nmg = {
   "applications": {
     "Vokou": {
@@ -9,7 +12,7 @@ var nmg = {
       "link": "Vokou"
     }
   }
-}
+};
 
 var pro = {
   "applications": {
@@ -20,14 +23,32 @@ var pro = {
       "link": "Vokou-Web-Pro"
     }
   }
-}
-stream.once('open', function(fd) {
+};
 
-  if(process.env.V_ENV == 'PRO'){
-    stream.write(JSON.stringify(pro));
+parseStream.once('open', function (fd) {
+
+  if (process.env.V_ENV == 'PRO') {
+    parseStream.write(JSON.stringify(pro));
+  } else {
+    parseStream.write(JSON.stringify(nmg));
   }
-  else{
-    stream.write(JSON.stringify(nmg));
-  }
-  stream.end();
+  parseStream.end();
 });
+
+var indexStream = fs.createWriteStream("./public/index.html");
+
+indexStream.once('open', function (fd) {
+  var vendor = '';
+  var bundle = '';
+  if (process.env.V_ENV == 'PRO') {
+    vendor = 'https://s3.amazonaws.com/vokou-pro/vendors.js';
+    bundle = 'https://s3.amazonaws.com/vokou-pro/bundle.js';
+  } else {
+    vendor = 'https://s3.amazonaws.com/vokou/vendors.js';
+    bundle = 'https://s3.amazonaws.com/vokou/bundle.js';
+  }
+  var htmlTemplate = "<!DOCTYPE html>\n<html lang=\"en\">\n  <head>\n    <meta charset=\"UTF-8\">\n    <title>Vokou</title>\n    <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css\">\n    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300,500' rel='stylesheet' type='text/css'>\n    <link rel=\"stylesheet\" href=\"./style.css\">\n    <script src=\"http://code.jquery.com/jquery-latest.min.js\"></script>\n  </head>\n  <body>\n    <div id=\"app\"></div>\n    <script src=\"" + vendor + "\"></script>\n    <script src=\"" + bundle + "\"></script>\n  </body>\n</html>\n";
+  indexStream.write(htmlTemplate);
+  indexStream.end();
+});
+
